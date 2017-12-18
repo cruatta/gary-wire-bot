91a6869490704fec818fc5591d051199d02de210
Cameron Ruatta <cameron@ruatta.com>
2017-12-17 19:04:02 -0800

    Add conf and project sub directory

--- src/main/scala/com/ruatta/gary/Config.scala
+++ src/main/scala/com/ruatta/gary/Config.scala
@@ -4,5 +4,12 @@ import com.wire.bots.sdk.Configuration
 
 class Config extends Configuration {
 
-  val getAdminHandle: Option[String] = sys.env.get("GARY_ADMIN_HANDLE")
+  val getAdminHandle: Option[String] = {
+    try {
+      Some(Configuration.propOrEnv("adminHandle", "GARY_ADMIN_HANDLE"))
+    } catch {
+      case _: Exception => None
+    }
+
+  }
 }
